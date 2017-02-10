03f770f7d0123a773b87d244b6cbbf8715e4fb2c
Matthew Gordon <laxredemption5525@gmail.com>
2017-02-10 16:23:18 -0700

    Add ui-router

--- src/client/app.module.js
+++ src/client/app.module.js
@@ -3,8 +3,22 @@
 
   angular
     .module('app', [
+      'ui.router',
       'home.component',
       'about.component'
-    ]);
+    ])
+    .config(config);
 
+    config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+
+    function config($stateProvider, $urlRouterProvider, $locationProvider) {
+      $locationProvider.html5(true);
+
+      $stateProvider
+        .state({
+          name: 'home',
+          url: '/',
+          component: 'home'
+        });
+    }
 }());
